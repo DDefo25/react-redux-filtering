28c20399a195eb1cf16dd903cfbd99e12ddc6b50
Maksim Pumé <94569453+DDefo25@users.noreply.github.com>
2023-06-09 10:06:26 +0000

    change input filter name

--- src/components/ServiceFilter.js
+++ src/components/ServiceFilter.js
@@ -7,7 +7,7 @@ export default function ServiceFilter() {
  
     return (
         <div className="position-relative m-3">
-                <input type="text" className="form-control" name='name' onChange={(evt) => dispatch(changeFilterField(evt.target.value))} value={filterField} />
+                <input type="text" className="form-control" name='filter' onChange={(evt) => dispatch(changeFilterField(evt.target.value))} value={filterField} />
                 <i className="bi bi-x position-absolute top-50 end-0 translate-middle-y" onClick={() => dispatch(clearFilterField())} />
         </div>
     );
